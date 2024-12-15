2167662aa818a06f1ca37888afc1043837c10791
Burair Ahmed <burair.s3380@gmail.com>
2024-12-16 02:33:22 +0500

    `Update Google Maps iframe src to use embed URL`

--- src/components/contact/ContactMap.jsx
+++ src/components/contact/ContactMap.jsx
@@ -6,7 +6,7 @@ const ContactMap = () => {
             <div className="maps-area bg-gray overflow-hidden">  
                 <div className="google-maps">  
                 <iframe
-    src="https://www.google.com/maps/place/Plano,+TX,+USA/@33.0613788,-96.8190279,19165m/data=!3m2!1e3!4b1!4m6!3m5!1s0x864c21da13c59513:0x62aa036489cd602b!8m2!3d33.0198431!4d-96.6988856!16zL20vMDEzbTR2?entry=ttu&g_ep=EgoyMDI0MTIxMS4wIKXMDSoASAFQAw%3D%3D"
+    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d107002.02923342277!2d-96.81902761416582!3d33.06137878361218!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x864c21da13c59513%3A0x62aa036489cd602b!2sPlano%2C%20TX%2C%20USA!5e0!3m2!1sen!2s!4v1734298334476!5m2!1sen!2s"
     width="600"
     height="450"
     style={{ border: 0 }}

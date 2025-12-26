641e0434ee96f3a228069533841838b7cff817bc
Burair Ahmed <62212433+burair-ahmed@users.noreply.github.com>
2025-12-26 18:14:46 +0500

    Update Google Maps iframe source to new location

--- src/components/contact/ContactMap.jsx
+++ src/components/contact/ContactMap.jsx
@@ -6,7 +6,7 @@ const ContactMap = () => {
             <div className="maps-area bg-gray overflow-hidden">  
                 <div className="google-maps">  
                 <iframe
-    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d107002.02923342277!2d-96.81902761416582!3d33.06137878361218!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x864c21da13c59513%3A0x62aa036489cd602b!2sPlano%2C%20TX%2C%20USA!5e0!3m2!1sen!2s!4v1734298334476!5m2!1sen!2s"
+    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1983.651619617065!2d-95.56942662243064!3d29.661580256931696!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8640e7fb3b5ec7f5%3A0xb8f75f8687573849!2s11011%20S%20Wilcrest%20Dr%20g210%2C%20Houston%2C%20TX%2077099%2C%20USA!5e1!3m2!1sen!2s!4v1766754821729!5m2!1sen!2s"
     width="600"
     height="450"
     style={{ border: 0 }}
